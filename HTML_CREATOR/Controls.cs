48f041cbfab3eeed5435880bfbb9654dcdb1fe74
Andy Gybels <outlawsbba@hotmail.com>
2016-10-18 14:51:08 +0200

    Combobox

---
+++ HTML_CREATOR/Controls.cs
@@ -0,0 +1,38 @@
+﻿namespace HTML_CREATOR
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+    class Controls
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _code;
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public static List<Controls> ControlList
+        {
+            get
+            {
+                return (
+                    from e in XDocument.Load(@"controls.xml").Root.Elements("control")
+                    select new Controls
+                    {
+                        Name = (string)e.Element("name"),
+                        Code = (string)e.Element("code")
+                    }).ToList();
+            }
+        }
+    }
+}
---
+++ HTML_CREATOR/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTML_CREATOR
+{
+    public enum HtmlLijst
+    {
+        Voornaam, Naam, Geslacht, Gehuwd, Geboortedatum,
+        Studie, Email, Straat, Huisnummer, Postcode,
+        Gemeente, Land, Telefoon
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.AddRange(typeof(HtmlLijst).GetEnumNames());
+            comboBox1.Items.AddRange(HTML_CREATOR.Controls.ControlList.ToArray());
+            comboBox1.DisplayMember = "Name";          
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FotoButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ofd.Title = "Open Image";
+                ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png" + "|All files (*.*)|*.*";
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox2.Text)) comboBox1.Enabled = true;
+            else comboBox1.Enabled = false;
+        }
+    }
+}
